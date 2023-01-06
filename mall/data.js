222f42c79cf61096c3b5281e7dd5b3b46a5cdd65
hndmin <andysjhan2@naver.com>
2023-01-07 02:32:46 +0900

    dongmin'sPush

---
+++ mall/data.js
@@ -0,0 +1,522 @@
+var pdData = [
+  {   
+    "category" : "수산&정육",  
+    "name" : "수림정 보리굴비 10미",
+      "ddockBiLabel" : false,
+      "discript" : "전남 화순에서 자연바람으로 말린 보리굴비",
+      "sale" : "",
+      "price" : "88,000원",
+      "link" : "https://notioniframe.com/notion/2dxbis9593l",
+      "shipment" : "당일 발송 혹은 배송일자 지정",
+      "badge" : [
+        {
+          "name" : "특가",
+          "color" : "#12e25f",
+          "show" : true
+        },
+        {
+          "name" : "기업(단체)구매 환영",
+          "color" : "#ff4f4f",
+          "show" : true
+        },
+        {
+          "name" : "무료배송",
+          "color" : "#b3b3b3",
+          "show" : true
+        },
+        {
+          "name" : "인터넷 최저가",
+          "color" : "#fdff00",
+          "show" : false
+        }
+      ]
+    },
+    {   
+      "category" : "수산&정육",  
+      "name" : "한우 1+ 선물세트 1.2kg",
+        "ddockBiLabel" : false,
+        "discript" : "한우 1+ 꽃등심/안심/채끝 각 300g",
+        "sale" : "",
+        "price" : "219,000원",
+        "link" : "https://notioniframe.com/notion/2krf6n2smab",
+        "shipment" : "당일 발송 혹은 배송일자 지정",
+        "badge" : [
+          {
+            "name" : "특가",
+            "color" : "#12e25f",
+            "show" : true
+          },
+          {
+            "name" : "기업(단체)구매 환영",
+            "color" : "#ff4f4f",
+            "show" : true
+          },
+          {
+            "name" : "무료배송",
+            "color" : "#b3b3b3",
+            "show" : true
+          },
+          {
+            "name" : "인터넷 최저가",
+            "color" : "#fdff00",
+            "show" : false
+          }
+        ]
+      },
+      {   
+        "category" : "수산&정육",  
+        "name" : "한우 1등급 선물세트 1.5kg",
+          "ddockBiLabel" : false,
+          "discript" : "한우 1등급 불고기/등심/안심/갈비살 각 400g",
+          "sale" : "",
+          "price" : "139,000원",
+          "link" : "https://notioniframe.com/notion/1rjkya86vg4",
+          "shipment" : "당일 발송 혹은 배송일자 지정",
+          "badge" : [
+            {
+              "name" : "특가",
+              "color" : "#12e25f",
+              "show" : true
+            },
+            {
+              "name" : "기업(단체)구매 환영",
+              "color" : "#ff4f4f",
+              "show" : true
+            },
+            {
+              "name" : "무료배송",
+              "color" : "#b3b3b3",
+              "show" : true
+            },
+            {
+              "name" : "인터넷 최저가",
+              "color" : "#fdff00",
+              "show" : false
+            }
+          ]
+        },
+        {   
+          "category" : "수산&정육",  
+          "name" : "LA갈비 선물세트 2.7kg",
+            "ddockBiLabel" : false,
+            "discript" : "미국산 초이스 등급",
+            "sale" : "",
+            "price" : "109,000원",
+            "link" : "https://notioniframe.com/notion/1yxt7cjla5u",
+            "shipment" : "당일 발송 혹은 배송일자 지정",
+            "badge" : [
+              {
+                "name" : "특가",
+                "color" : "#12e25f",
+                "show" : true
+              },
+              {
+                "name" : "기업(단체)구매 환영",
+                "color" : "#ff4f4f",
+                "show" : true
+              },
+              {
+                "name" : "무료배송",
+                "color" : "#b3b3b3",
+                "show" : true
+              },
+              {
+                "name" : "인터넷 최저가",
+                "color" : "#fdff00",
+                "show" : false
+              }
+            ]
+          },
+          {   
+            "category" : "수산&정육",  
+            "name" : "1++ 한우 구이 세트",
+              "ddockBiLabel" : false,
+              "discript" : "",
+              "sale" : "",
+              "price" : "330,000원",
+              "link" : "https://notioniframe.com/notion/2h0budmsxc7",
+              "shipment" : "당일 발송 혹은 배송일자 지정",
+              "badge" : [
+                {
+                  "name" : "특가",
+                  "color" : "#12e25f",
+                  "show" : true
+                },
+                {
+                  "name" : "기업(단체)구매 환영",
+                  "color" : "#ff4f4f",
+                  "show" : true
+                },
+                {
+                  "name" : "무료배송",
+                  "color" : "#b3b3b3",
+                  "show" : true
+                },
+                {
+                  "name" : "인터넷 최저가",
+                  "color" : "#fdff00",
+                  "show" : false
+                }
+              ]
+            },
+          //////
+          {   
+            "category" : "과일",  
+            "name" : "사과 배 혼합 선물세트",
+              "ddockBiLabel" : false,
+              "discript" : "사과 6개 + 배 4개(총 3.6kg)",
+              "sale" : "",
+              "price" : "41,900원",
+              "link" : "https://notioniframe.com/notion/um6v06zxgor",
+              "shipment" : "당일 발송 혹은 배송일자 지정",
+              "badge" : [
+                {
+                  "name" : "특가",
+                  "color" : "#12e25f",
+                  "show" : true
+                },
+                {
+                  "name" : "기업(단체)구매 환영",
+                  "color" : "#ff4f4f",
+                  "show" : true
+                },
+                {
+                  "name" : "무료배송",
+                  "color" : "#b3b3b3",
+                  "show" : false
+                },
+                {
+                  "name" : "보자기 포장(추가금액)",
+                  "color" : "#fdff00",
+                  "show" : true
+                }
+              ]
+            },
+            {   
+              "category" : "과일",  
+              "name" : "사과 배 한라봉 혼합 선물세트",
+                "ddockBiLabel" : false,
+                "discript" : "한라봉 3개 + 사과 3개 + 배 3개(총 4kg)",
+                "sale" : "",
+                "price" : "45,900원",
+                "link" : "https://notioniframe.com/notion/u11lex4nvk",
+                "shipment" : "당일 발송 혹은 배송일자 지정",
+                "badge" : [
+                  {
+                    "name" : "특가",
+                    "color" : "#12e25f",
+                    "show" : true
+                  },
+                  {
+                    "name" : "기업(단체)구매 환영",
+                    "color" : "#ff4f4f",
+                    "show" : true
+                  },
+                  {
+                    "name" : "무료배송",
+                    "color" : "#b3b3b3",
+                    "show" : false
+                  },
+                  {
+                    "name" : "보자기 포장(추가금액)",
+                    "color" : "#fdff00",
+                    "show" : true
+                  }
+                ]
+              },
+              {   
+                "category" : "과일",  
+                "name" : "청도 반건시 24개입",
+                  "ddockBiLabel" : false,
+                  "discript" : "특가, 기업(단체)구매 환영, 무료배송",
+                  "sale" : "",
+                  "price" : "42,900원",
+                  "link" : "https://notioniframe.com/notion/16gwc4ob33b",
+                  "shipment" : "당일 발송 혹은 배송일자 지정",
+                  "badge" : [
+                    {
+                      "name" : "특가",
+                      "color" : "#12e25f",
+                      "show" : true
+                    },
+                    {
+                      "name" : "기업(단체)구매 환영",
+                      "color" : "#ff4f4f",
+                      "show" : true
+                    },
+                    {
+                      "name" : "무료배송",
+                      "color" : "#b3b3b3",
+                      "show" : true
+                    },
+                    {
+                      "name" : "인터넷 최저가",
+                      "color" : "#fdff00",
+                      "show" : false
+                    }
+                  ]
+                },
+                {   
+                  "category" : "과일",  
+                  "name" : "상주 곶감 24개입",
+                    "ddockBiLabel" : false,
+                    "discript" : "낱개포장으로 간편한 섭취 가능",
+                    "sale" : "",
+                    "price" : "34,900원",
+                    "link" : "https://notioniframe.com/notion/21g0e4l65kz",
+                    "shipment" : "당일 발송 혹은 배송일자 지정",
+                    "badge" : [
+                      {
+                        "name" : "특가",
+                        "color" : "#12e25f",
+                        "show" : true
+                      },
+                      {
+                        "name" : "기업(단체)구매 환영",
+                        "color" : "#ff4f4f",
+                        "show" : true
+                      },
+                      {
+                        "name" : "무료배송",
+                        "color" : "#b3b3b3",
+                        "show" : true
+                      },
+                      {
+                        "name" : "인터넷 최저가",
+                        "color" : "#fdff00",
+                        "show" : false
+                      }
+                    ]
+                  },
+                  //
+                  {   
+                    "category" : "과일",  
+                    "name" : "제주 레드향 선물세트 3Kg",
+                      "ddockBiLabel" : false,
+                      "discript" : "대과 8-12개",
+                      "sale" : "",
+                      "price" : "42,500원",
+                      "link" : "https://notioniframe.com/notion/1o2f7dheb7rd",
+                      "shipment" : "당일 발송 혹은 배송일자 지정",
+                      "badge" : [
+                        {
+                          "name" : "특가",
+                          "color" : "#12e25f",
+                          "show" : true
+                        },
+                        {
+                          "name" : "기업(단체)구매 환영",
+                          "color" : "#ff4f4f",
+                          "show" : true
+                        },
+                        {
+                          "name" : "무료배송",
+                          "color" : "#b3b3b3",
+                          "show" : false
+                        },
+                        {
+                          "name" : "보자기 포장(추가금액)",
+                          "color" : "#fdff00",
+                          "show" : true
+                        }
+                      ]
+                    },
+                    {   
+                      "category" : "과일",  
+                      "name" : "제주 한라봉 선물세트 4.5kg",
+                        "ddockBiLabel" : false,
+                        "discript" : "14-16개",
+                        "sale" : "",
+                        "price" : "46,000원",
+                        "link" : "https://notioniframe.com/notion/1lpu3yoy4qt",
+                        "shipment" : "당일 발송 혹은 배송일자 지정",
+                        "badge" : [
+                          {
+                            "name" : "특가",
+                            "color" : "#12e25f",
+                            "show" : true
+                          },
+                          {
+                            "name" : "기업(단체)구매 환영",
+                            "color" : "#ff4f4f",
+                            "show" : true
+                          },
+                          {
+                            "name" : "무료배송",
+                            "color" : "#b3b3b3",
+                            "show" : false
+                          },
+                          {
+                            "name" : "보자기 포장(추가금액)",
+                            "color" : "#fdff00",
+                            "show" : true
+                          }
+                        ]
+                      },
+                      {   
+                        "category" : "과일",  
+                        "name" : "나주배 선물세트 7.5Kg",
+                          "ddockBiLabel" : false,
+                          "discript" : "9-11개",
+                          "sale" : "",
+                          "price" : "41,400원",
+                          "link" : "https://notioniframe.com/notion/sb4jfc706i",
+                          "shipment" : "당일 발송 혹은 배송일자 지정",
+                          "badge" : [
+                            {
+                              "name" : "특가",
+                              "color" : "#12e25f",
+                              "show" : true
+                            },
+                            {
+                              "name" : "기업(단체)구매 환영",
+                              "color" : "#ff4f4f",
+                              "show" : true
+                            },
+                            {
+                              "name" : "무료배송",
+                              "color" : "#b3b3b3",
+                              "show" : false
+                            },
+                            {
+                              "name" : "보자기 포장(추가금액)",
+                              "color" : "#fdff00",
+                              "show" : true
+                            }
+                          ]
+                        },
+                        
+  ////////////////
+  {   
+    "category" : "건강식품",  
+    "name" : "초신선 기름세트",
+      "ddockBiLabel" : false,
+      "discript" : "참기름/들기름/생들기름 세트",
+      "sale" : "",
+      "price" : "60,900원",
+      "link" : "https://notioniframe.com/notion/2nsr15jioci",
+      "shipment" : "당일 발송 혹은 배송일자 지정",
+      "badge" : [
+        {
+          "name" : "특가",
+          "color" : "#12e25f",
+          "show" : true
+        },
+        {
+          "name" : "기업(단체)구매 환영",
+          "color" : "#ff4f4f",
+          "show" : true
+        },
+        {
+          "name" : "무료배송",
+          "color" : "#b3b3b3",
+          "show" : true
+        },
+        {
+          "name" : "인터넷 최저가",
+          "color" : "#fdff00",
+          "show" : false
+        }
+      ]
+    },
+    {   
+      "category" : "건강식품",  
+      "name" : "정관장 에브리타임 밸런스",
+        "ddockBiLabel" : false,
+        "discript" : "스틱홍삼(30포, 1개월 분)",
+        "sale" : "",
+        "price" : "59,900원",
+        "link" : "https://notioniframe.com/notion/1th4o4gb4ve",
+        "shipment" : "당일 발송 혹은 배송일자 지정",
+        "badge" : [
+          {
+            "name" : "특가",
+            "color" : "#12e25f",
+            "show" : true
+          },
+          {
+            "name" : "기업(단체)구매 환영",
+            "color" : "#ff4f4f",
+            "show" : true
+          },
+          {
+            "name" : "무료배송",
+            "color" : "#b3b3b3",
+            "show" : true
+          },
+          {
+            "name" : "인터넷 최저가",
+            "color" : "#fdff00",
+            "show" : false
+          }
+        ]
+      },
+      {   
+        "category" : "건강식품",  
+        "name" : "정관장 홍삼정",
+          "ddockBiLabel" : false,
+          "discript" : "100% 홍삼농축액 240g",
+          "sale" : "",
+          "price" : "189,000원",
+          "link" : "https://notioniframe.com/notion/2kbfhzffmra",
+          "shipment" : "당일 발송 혹은 배송일자 지정",
+          "badge" : [
+            {
+              "name" : "특가",
+              "color" : "#12e25f",
+              "show" : true
+            },
+            {
+              "name" : "기업(단체)구매 환영",
+              "color" : "#ff4f4f",
+              "show" : true
+            },
+            {
+              "name" : "무료배송",
+              "color" : "#b3b3b3",
+              "show" : true
+            },
+            {
+              "name" : "인터넷 최저가",
+              "color" : "#fdff00",
+              "show" : false
+            }
+          ]
+        },
+        {   
+          "category" : "건강식품",  
+          "name" : "정관장 활기력",
+            "ddockBiLabel" : false,
+            "discript" : "20ml, 16개입",
+            "sale" : "",
+            "price" : "37,300원",
+            "link" : "https://notioniframe.com/notion/2g4qwvet5lw",
+            "shipment" : "당일 발송 혹은 배송일자 지정",
+            "badge" : [
+              {
+                "name" : "특가",
+                "color" : "#12e25f",
+                "show" : true
+              },
+              {
+                "name" : "기업(단체)구매 환영",
+                "color" : "#ff4f4f",
+                "show" : true
+              },
+              {
+                "name" : "무료배송",
+                "color" : "#b3b3b3",
+                "show" : true
+              },
+              {
+                "name" : "인터넷 최저가",
+                "color" : "#fdff00",
+                "show" : false
+              }
+            ]
+          }
+    
+]
+
+
+export var Data = pdData;
---
+++ mall/script.js
@@ -0,0 +1,159 @@
+
+
+
+
+//import {Data} from './data.js';
+var pdData;
+var pdDataLen;
+
+
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+function copyText() {
+  var ProductName = document.getElementsByClassName("pdName")[0].innerText;
+  var copyText = "똑비~! " + ProductName + " 구매해주세요";
+  navigator.clipboard.writeText(copyText);
+}
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+// pdName / pdDiscript / pdSale / pdPrice / pdLink
+
+//var pdDataLen = pdData.length;
+function mainOnload(Data){
+  pdData = Data;
+  pdDataLen = pdData.length;
+
+
+  // 상품 노드 생성 및 복사 
+  var pdNode = document.getElementsByClassName("product")[0];
+  var pdList = document.getElementById("pdList");
+  for(i=0; i<pdDataLen - 1; i++){
+    pdList.appendChild(pdNode.cloneNode(true));
+  }
+
+
+  var product = document.getElementsByClassName("product");
+  //console.log(product);
+  for(i=0; i<pdDataLen; i++){
+    // 상품 컨텐츠 채우기
+    //product[i].href = "./productPages/" + i + ".html";
+    product[i].href = "./" + "detail" + ".html";
+    product[i].setAttribute('onclick','toLocal('+ i +')');
+
+    product[i].getElementsByClassName("pdImg")[0].src = "./src/img/product/" + i + ".png";
+    product[i].getElementsByClassName("pdName")[0].innerText = pdData[i]["name"];
+    product[i].getElementsByClassName("pdDiscript")[0].innerText = pdData[i]["discript"];
+    product[i].getElementsByClassName("pdSale")[0].innerText = pdData[i]["sale"];
+    product[i].getElementsByClassName("pdPrice")[0].innerText = pdData[i]["price"];
+
+    // 똑비라벨 붙이기
+    if(pdData[i]["ddockBiLabel"] == false){
+      product[i].getElementsByClassName("pdDockLabel")[0].style.opacity = "0";
+    }
+
+    //상품 뱃지 붙이기
+    var badgeLenth = pdData[i]["badge"].length;
+    for(j=0; j<badgeLenth; j++){
+      if(pdData[i]["badge"][j]["show"] == true){
+        var newBadge = document.createElement("span");
+        var badgeText = document.createTextNode(pdData[i]["badge"][j]["name"]);
+        newBadge.appendChild(badgeText);
+        newBadge.classList.add("badge");
+        newBadge.style.backgroundColor = pdData[i]["badge"][j]["color"]
+
+        product[i].getElementsByClassName("badgeGroup")[0].append(newBadge);
+      }
+    }
+
+    //
+  }
+}
+//////////////////////////////////////////////////////////////////////////////
+//myStorage = window.localStorage;
+function toLocal(i){
+  window.localStorage.setItem("pdIndex", i);
+}
+//////////////////////////////////////////////////////////////////////////////
+function detailOnload(Data, i){
+  //console.log("key :" + window.localStorage.getItem("pdIndex"));
+
+  pdData = Data;
+  pdDataLen = pdData.length;
+
+
+  var product = document.getElementsByClassName("product");
+  
+  product[0].getElementsByClassName("pdImg")[0].src = "./src/img/product/" + i + ".png"
+  product[0].getElementsByClassName("pdName")[0].innerText = pdData[i]["name"];
+  product[0].getElementsByClassName("pdDiscript")[0].innerText = pdData[i]["discript"];
+  product[0].getElementsByClassName("pdSale")[0].innerText = pdData[i]["sale"];
+  product[0].getElementsByClassName("pdPrice")[0].innerText = pdData[i]["price"];
+  product[0].getElementsByClassName("info1")[0].innerText = pdData[i]["shipment"];
+  //
+  document.getElementById("pdDetail").src = pdData[i]["link"];
+  
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+function categoryFilter(x){
+  var btns = document.getElementById("categoryList").getElementsByClassName("btn");
+  var btnLength = btns.length;
+  var key = {};
+  for(i=0; i<btnLength; i++){
+    btns[i].classList.remove("on");
+  }
+  btns[x].classList.add("on");
+
+
+  var product = document.getElementsByClassName("product");
+  switch (x) {
+    case 0:
+      for(i=0; i<pdDataLen; i++){
+        product[i].style.display = "block";
+      }
+      break;
+    case 1:
+      for(i=0; i<pdDataLen; i++){
+        if(pdData[i]["category"] == "수산&정육"){
+          product[i].style.display = "block";
+        }else{
+          product[i].style.display = "none";
+        }
+      }
+      break;
+    case 2:
+      for(i=0; i<pdDataLen; i++){
+        if(pdData[i]["category"] == "과일"){
+          product[i].style.display = "block";
+        }else{
+          product[i].style.display = "none";
+        }
+      }
+      break;
+    case 3:
+      for(i=0; i<pdDataLen; i++){
+        if(pdData[i]["category"] == "건강식품"){
+          product[i].style.display = "block";
+        }else{
+          product[i].style.display = "none";
+        }
+      }
+      break;
+    default:
+      alert( "" );
+  }
+}
+
+  
